ea66cd3ec7498cbd0947b4ef5cd3e8bd52b57a41
corosk <corosk>
2016-12-31 23:36:02 +0900

    Add multiple hierarchy addition

--- js/main.js
+++ js/main.js
@@ -1,23 +1,58 @@
 /* 処理は必ずここ中に書く */
 $(function(){
-  /***
-   * ファイルリスト取得メソッド
-   */
   $(".getfile").click(function(){
-    var dir = $(".targetDir").val();
     $.ajax({
       type: 'GET',
       url: '/webmanager/api/filelist.php',
       dataType: 'json',
       data:{
-        'dir':dir
+        'dir': $(".targetDir").val()
       },
       success: function(response) {
         $(".filelist").html(response);
+        getDirForAjax();
       },
       error: function(response) {
         console.log(response);
       }
     });
   });
+  /***
+   * ファイルリスト取得メソッド
+   */
+   function getDirForAjax(target){
+     let eventT = $(".dir");
+     if(typeof target != "undefined"){
+       eventT = target;
+     }
+     eventT.on("click",function(){
+       let origin = $(this);
+       let dir = origin.data("dir");
+       $.ajax({
+         type: 'GET',
+         url: '/webmanager/api/filelist.php',
+         dataType: 'json',
+         data:{
+           'dir':dir
+         },
+         success: function(response) {
+           origin.parent("li").append(response);
+           origin.val("-");
+           getDirForAjax(origin.siblings().find("input[class='dir']"));
+           origin.unbind();
+           origin.click(function(){
+             $(this).siblings("ul:first").toggle();
+             if($(this).siblings("ul:first").css("display") === "none" ){
+               $(this).val("+");
+             }else{
+               $(this).val("-");
+             }
+           });
+         },
+         error: function(response) {
+           console.log(response);
+         }
+       });
+     });
+   }
 });
--- service/file.php
+++ service/file.php
@@ -9,10 +9,10 @@ function getFileList($dir) {
     $list = array();
     foreach ($files as $file) {
         if (is_file($file)) {
-            $list[] = "<input type='checkbox'/>".$file;
+            $list[] = "<input type='checkbox' value='".$file."'/>".$file;
         }
         if (is_dir($file)) {
-            $list[] = "<input type='button' value='+'/>".$file;
+            $list[] = "<input type='button' class='dir' data-dir='".$file."' value='+'/>".$file;
         }
     }
     return $list;
